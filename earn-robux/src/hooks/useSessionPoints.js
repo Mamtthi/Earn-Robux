6624844a45da5cf04a5ca9b43650782a861d219d
Mamtthi <matthias_nass@web.de>
2025-10-21 18:41:50 +0200

    URL der Punkte.php aktualisiert

--- earn-robux/src/hooks/useSessionPoints.js
+++ earn-robux/src/hooks/useSessionPoints.js
@@ -5,7 +5,7 @@ export default function useSessionPoints( initialValue = 0 ){
     const [sessionPoints,setSessionPoints] = useState(initialValue);
     const [totalPoints, setTotalPoints] = useState(0);
 
-    const PHP_URL = "https://fastpractice.social-networking.me/punkte.php";
+    const PHP_URL = "https://amonslernseite.loveslife.biz/punkte.php";
 
         useEffect(() => {
             const fetchPoints = async () => {
