c11614d77c8008240b9c7a2e820a30d981e7a0ea
Mamtthi <matthias_nass@web.de>
2025-10-24 18:07:09 +0200

    Update Verce value.js
    update single to maybesingle()

--- earn-robux/api/value.js
+++ earn-robux/api/value.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
       .from('Points')
       .select('points')
       .eq('id', 1)
-      .single();
+      .maybeSingle();
 
     console.log("DATA:", data, "ERROR:", error);
 
